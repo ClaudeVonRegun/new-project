930239ff6dfd2a81370127905c971fc98737e9d9
ClaudeVonRegun <claude7795@gmail.com>
2024-11-08 10:28:04 +0900

    lesson1.phpの追加

---
+++ bunki.php
@@ -0,0 +1,17 @@
+<?php
+  // 分岐処理
+  // 分岐処理は、if文を使うことで分けることができます。
+  $score = 89;
+  if($score >=90) {
+    echo "AA";
+  }else if($score >=80){
+    echo "A";
+  }else if($score >=70){
+    echo "B";
+  }else if($score >=60){
+    echo "C";
+  }else{
+    echo "D";
+  }
+  echo "\n";
+?>
---
+++ lesson1.php
@@ -0,0 +1,31 @@
+<?php
+  // 問１
+  $message = "Hello World";
+  echo $message;
+  echo "\n";
+  // 問2
+  $name = "Satoshi Kuroda";
+  echo "Welcome ". $name;
+  echo "\n";
+  // 問3
+  $apple_price = 200;
+  $apple_quantity = 3;
+  $orange_price = 100;
+  $orange_quantity = 4;
+  $total = $apple_price * $apple_quantity + $orange_price * $orange_quantity;
+  echo $total."円";
+  echo "\n";
+  // 問４
+  $colors = ["red", "blue", "green"];
+  echo $colors[1];
+  echo "\n";
+  // 問５
+  $members =[
+    ["name" => "佐藤", "age" => 36, "job" => "営業"],
+    ["name" => "田中", "age" => 23, "job" => "事務"],
+    ["name" => "吉田", "age" => 54, "job" => "社長"],
+  ];
+  echo "田中さんは".$members[1]["age"]."歳です。";
+  // 田中さんのインデックス番号[1]を取得して、keyである"age"からvalueの23を表示させる。
+  // 「田中さんの年齢」のため、array_serchやarray_filterなどを使うべきか迷いました。
+?>
