00a9aa6d069bc5df23e6d184a227ed9b7f74d13b
ClaudeVonRegun <claude7795@gmail.com>
2025-01-09 14:54:52 +0900

    lesson3.phpの追加

--- lesson3.php
+++ lesson3.php
@@ -1,26 +1,21 @@
 <?php
 // 問１
-$i = 1;
 for($i = 1; $i <= 10; $i++){
   echo $i;
   echo "\n";
 }
 
 // 問２
-$i = 1;
 $sum = 0;
-while($i <= 50){
+for($i = 1; $i <= 50; $i++){
   $sum += $i; 
-  $i++;
 }
-echo $sum;
+echo "1から50の合計は".$sum."です";
 echo "\n";
 
 // 問３
-$i = 10;
-while($i >= 1){
+for($i = 10; $i >= 1; $i--){
   echo $i;
   echo "\n";
-  $i--;
 }
 ?>
